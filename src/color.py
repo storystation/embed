609b29eaa1665beef52ffdaa04e9c8ac76e61c4d
Leamundis <jonathan.peraudon@yahoo.fr>
2019-08-28 15:27:20 +0200

    Update script : work live

--- src/color.py
+++ src/color.py
@@ -2,55 +2,121 @@
 import time
 import RPi.GPIO as GPIO
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-
-# Define buttons
-GPIO.setup(11,GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Blue button
-GPIO.setup(13,GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Red button
-GPIO.setup(15,GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Green button
-
-# Define LED
-GPIO.setup(36,GPIO.OUT) # Blue LED
-GPIO.setup(38,GPIO.OUT) # Red LED
-GPIO.setup(40,GPIO.OUT) # Green LED
-
-right_anwser = ??? #TODO To get from API
-
 answers = {
     # [blue, red, green]
     "red": [0,1,0],
     "blue": [1,0,0],
     "green": [0,0,1],
-    "mangeta": [1,1,0],
+    "magenta": [1,1,0],
     "yellow": [0,1,1],
     "cyan": [1,0,1],
     "white": [1,1,1]
 }
 
+def startLed(win):
+    GPIO.setmode(GPIO.BOARD)
+    ledGreenPin = 35      #define 3 pins of RGBLED
+    ledRedPin = 33
+    ledBluePin = 37
+    GPIO.setup(ledRedPin,GPIO.OUT)      #set 3 pins of RGBLED to output mode
+    GPIO.setup(ledGreenPin,GPIO.OUT)
+    GPIO.setup(ledBluePin,GPIO.OUT)
+    p_Red = GPIO.PWM(ledRedPin,1000)    #configure PMW to 3 pins of RGBLED
+    p_Red.start(0)
+    p_Green = GPIO.PWM(ledGreenPin,1000)
+    p_Green.start(0)
+    p_Blue = GPIO.PWM(ledBluePin,1000)
+    p_Blue.start(0)
 
-def checkAnswer(arrayToCheck):
-    if answers[rightAnswer] == arrayToCheck:
-        return True #TODO Send True to API
+
+    right_anwser = win
+    if right_anwser == "magenta":
+        p_Red.ChangeDutyCycle(0)  #magenta
+        p_Green.ChangeDutyCycle(100)
+        p_Blue.ChangeDutyCycle(0)
+    elif right_anwser == "yellow":
+        p_Red.ChangeDutyCycle(0)  #jaune
+        p_Green.ChangeDutyCycle(0)
+        p_Blue.ChangeDutyCycle(100)
+    elif right_anwser == "cyan":
+        p_Red.ChangeDutyCycle(100)  #cyan
+        p_Green.ChangeDutyCycle(0)
+        p_Blue.ChangeDutyCycle(0)
+    elif right_anwser == "red":
+        p_Red.ChangeDutyCycle(0)  #rouge
+        p_Green.ChangeDutyCycle(100)
+        p_Blue.ChangeDutyCycle(100)
+    elif right_anwser == "blue":
+        p_Red.ChangeDutyCycle(100)  #bleu
+        p_Green.ChangeDutyCycle(100)
+        p_Blue.ChangeDutyCycle(0)
+    elif right_anwser == "green":
+        p_Red.ChangeDutyCycle(100)  #vert
+        p_Green.ChangeDutyCycle(0)
+        p_Blue.ChangeDutyCycle(100)
+    elif right_anwser == "white":
+        p_Red.ChangeDutyCycle(100)  #blanc
+        p_Green.ChangeDutyCycle(100)
+        p_Blue.ChangeDutyCycle(100)
+
+
+def turnOff():
+    GPIO.output(36,GPIO.LOW)
+    GPIO.output(38,GPIO.LOW)
+    GPIO.output(40,GPIO.LOW)
+
+def checkAnswer(arrayToCheck, win):
+    time.sleep(1)
+    turnOff()
+    GPIO.cleanup()
+    if answers[win] == arrayToCheck:
+        return "OK"
     else:
-        return False #TODO Send False to API
+        return "KO"
 
-# Infinite loop
-while True:
-    # Get buttons state
-    state_blue = GPIO.input(11)
-    state_red = GPIO.input(13)
-    state_green = GPIO.input(15)
-    
-    
-    # Check buttons state
-    if state_blue == 1 or state_red == 1 or state_green == 1:
+def start(win):
+    startLed(win)
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+
+    # Define buttons
+    GPIO.setup(11,GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Blue button
+    GPIO.setup(13,GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Red button
+    GPIO.setup(15,GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Green button
+
+    # Define LED
+    GPIO.setup(36,GPIO.OUT) # Blue LED
+    GPIO.setup(38,GPIO.OUT) # Red LED
+    GPIO.setup(40,GPIO.OUT) # Green LED
+
+    try:   
+        # Infinite loop
+        flag = 0
+        while True:
+            # Get buttons state
+            state_blue = GPIO.input(11)
+            state_red = GPIO.input(13)
+            state_green = GPIO.input(15)
+                
+            # Check buttons state  
+            if state_blue == 1 or state_red == 1 or state_green == 1:
+                flag = flag + 1
+                time.sleep(0.1)
+                if state_blue: GPIO.output(36,GPIO.HIGH)
+                if state_red: GPIO.output(38,GPIO.HIGH) 
+                if state_green: GPIO.output(40,GPIO.HIGH)
+                #print("Blue = ", state_blue)
+                #print("Green = ", state_green)
+                #print("Red = ", state_red)
+                #print("flag = ", flag)
+                #print("")
+                if flag == 3:
+                    checkAnswer([state_blue, state_red, state_green], win)
+            else:
+                GPIO.output(36,GPIO.LOW)
+                GPIO.output(38,GPIO.LOW)
+                GPIO.output(40,GPIO.LOW)
         time.sleep(0.1)
-        if state_blue: GPIO.output(36,GPIO.HIGH)
-        if state_blue: GPIO.output(38,GPIO.HIGH) 
-        if state_blue: GPIO.output(40,GPIO.HIGH) 
-
-        checkAnswer([state_blue, state_red, state_green])
-
-    time.sleep(0.1)
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+            
+    except KeyboardInterrupt:
+        GPIO.cleanup()
---
+++ src/distance.py
@@ -0,0 +1,47 @@
+# SCRIPT POUR RADAR DE DISTANCE
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+
+GPIO.setup(16,GPIO.OUT) # TRIG
+GPIO.setup(22,GPIO.IN) # ECHO
+
+GPIO.output(16,False)
+
+#repet = input("Entrez un nombre de repetitions de mesure : ")
+
+beginning = time.time()
+for x in range(50):
+    time.sleep(1)
+    
+    GPIO.output(16,True)
+    time.sleep(0.0001)
+    GPIO.output(16,False)
+    
+    while GPIO.input(22)==0:
+        debutImpulsion = time.time()
+    
+    while GPIO.input(22)==1:
+        finImpulsion = time.time()
+    
+    distance = round((finImpulsion - debutImpulsion) * 340 * 100 / 2, 1)
+
+    minDist = 10.0
+    maxDist = minDist + 10.0
+    
+    if distance > minDist and distance < maxDist:
+        print("Tu es as", distance, "cm depuis", int(time.time() - beginning), "secondes!")
+        if time.time() > beginning + 5.0:
+            print("Bonne distance!")
+            exit()
+
+    else: 
+        print("Tu es as ", distance, "cm")
+        beginning = time.time()
+
+    time.sleep(0.1)
+
+GPIO.cleanup()
